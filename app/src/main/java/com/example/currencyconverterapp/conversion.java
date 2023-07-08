9e2d42c6938b710ca75ac5e756fcfbd65d5b4af0
Ryan Elizondo-Fallas <ryanelizondo10@gmail.com>
2023-07-08 18:12:28 -0400

    updated UI & added HTTPS Request

--- app/src/main/java/com/example/currencyconverterapp/conversion.java
+++ app/src/main/java/com/example/currencyconverterapp/conversion.java
@@ -1,5 +1,12 @@
 package com.example.currencyconverterapp;
 
+
+import static com.example.currencyconverterapp.convertActivity.toAmount;
+
+import android.os.AsyncTask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -10,35 +17,48 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.DecimalFormat;
 
-public class conversion {
+public class conversion extends AppCompatActivity {
+    private DecimalFormat f = new DecimalFormat("00.00");
+    void sendHttpGetRequest(String convertFrom, String convertTo, double amount) {
+        new AsyncTask<Void, Void, Double>() {
+            @Override
+            protected Double doInBackground(Void... voids) {
+                try {
+                    String GET_URL = "https://v6.exchangerate-api.com/v6/7d20d4932f7305a978100fd5/pair/" + convertFrom + "/" + convertTo;
+                    URL url = new URL(GET_URL);
+                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                    httpURLConnection.setRequestMethod("GET");
+                    int responseCode = httpURLConnection.getResponseCode();
+                    if (responseCode == HttpURLConnection.HTTP_OK) {
+                        BufferedReader in = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
 
-    private static void sendHttpGETRequest(String convertFrom, String convertTo, double amount) throws IOException, JSONException {
+                        String inputLine;
+                        StringBuffer response = new StringBuffer();
 
-        DecimalFormat f = new DecimalFormat("00.00");
-        String GET_URL = "https://v6.exchangerate-api.com/v6/7d20d4932f7305a978100fd5/pair/" + convertFrom + "/" + convertTo;
-        URL url = new URL(GET_URL);
-        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-        httpURLConnection.setRequestMethod("GET");
-        int responseCode = httpURLConnection.getResponseCode();
+                        while ((inputLine = in.readLine()) != null) {
+                            response.append(inputLine);
+                        }
+                        in.close();
 
-        if(responseCode == HttpURLConnection.HTTP_OK){
-            BufferedReader in = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+                        JSONObject obj = new JSONObject(response.toString());
+                        return obj.getDouble("conversion_rate") * amount;
+                    }
+                } catch (IOException | JSONException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
 
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-
-            while((inputLine = in.readLine()) != null){
-                response.append(inputLine);
-            }in.close();
-
-            JSONObject obj = new JSONObject(response.toString());
-            Double exchangeRate = obj.getDouble("conversion_rate");
-            System.out.println();
-            System.out.println(f.format(amount) + " " + convertFrom + " = " + f.format(amount*exchangeRate) + " " + convertTo); //conversion result
-
-        }
-        else{
-            System.out.println("Get request failed");
-        }
+            @Override
+            protected void onPostExecute(Double result) {
+                if (result != null) {
+                    toAmount.setText(f.format(result));
+                } else {
+                    System.out.println("Get request failed");
+                }
+            }
+        }.execute();
     }
+
+
 }
--- app/src/main/java/com/example/currencyconverterapp/convertActivity.java
+++ app/src/main/java/com/example/currencyconverterapp/convertActivity.java
@@ -1,13 +1,13 @@
 package com.example.currencyconverterapp;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -18,74 +18,56 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.DecimalFormat;
 
+
 public class convertActivity  extends AppCompatActivity {
 
-        private Button convert;
-        private TextView fromLabel;
-        private TextView toLabel;
-        private TextView fromAmount;
-        private TextView toAmount;
+    private Button convert;
+    private Button back;
+    private TextView fromLabel;
+    private TextView toLabel;
+    static TextView toAmount;
+    private EditText fromAmount;
+
+    conversion conversion = new conversion();
 
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_convert);
+        convert = findViewById(R.id.convert);
+        back = findViewById(R.id.back);
+        fromLabel = findViewById(R.id.fromLabel);
+        toLabel = findViewById(R.id.toLabel);
+        toAmount = findViewById(R.id.toAmount);
+        fromAmount = findViewById(R.id.fromAmount);
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_convert);
-            convert = findViewById(R.id.convert);
-            fromLabel = findViewById(R.id.fromLabel);
-            toLabel = findViewById(R.id.toLabel);
-            fromAmount = findViewById(R.id.fromAmount);
-            toAmount = findViewById(R.id.toAmount);
-
-            String currency = getIntent().getStringExtra("currency");
-            if(currency != null) {
-                fromLabel.setText("USD");
-                toLabel.setText(currency);
-            }
-        }
-
-        public void convertButton(View view) {
-            String convertFrom = fromAmount.getText().toString();
-            String convertTo = toAmount.getText().toString();
-            Double amount = Double.parseDouble(fromAmount.getText().toString());
-            try {
-                sendHttpGETRequest(convertFrom, convertTo, amount);
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-
-    private static void sendHttpGETRequest(String convertFrom, String convertTo, double amount) throws IOException, JSONException {
-
-        DecimalFormat f = new DecimalFormat("00.00");
-        String GET_URL = "https://v6.exchangerate-api.com/v6/7d20d4932f7305a978100fd5/pair/" + convertFrom + "/" + convertTo;
-        URL url = new URL(GET_URL);
-        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-        httpURLConnection.setRequestMethod("GET");
-        int responseCode = httpURLConnection.getResponseCode();
-
-        if(responseCode == HttpURLConnection.HTTP_OK){
-            BufferedReader in = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
-
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-
-            while((inputLine = in.readLine()) != null){
-                response.append(inputLine);
-            }in.close();
-
-            JSONObject obj = new JSONObject(response.toString());
-            Double exchangeRate = obj.getDouble("conversion_rate");
-            System.out.println();
-            System.out.println(f.format(amount) + " " + convertFrom + " = " + f.format(amount*exchangeRate) + " " + convertTo); //conversion result
-
-        }
-        else{
-            System.out.println("Get request failed");
+        String currency = getIntent().getStringExtra("currency");
+        if (currency != null) {
+            fromLabel.setText("USD");
+            toLabel.setText(currency);
         }
     }
 
+    public void backButton(View view) {
+        Intent intent = new Intent(convertActivity.this, mainActivity.class);
+        convertActivity.this.startActivity(intent);
+    }
+
+
+    public void convertButton(View view) throws JSONException, IOException {
+        String convertFrom = fromLabel.getText().toString();
+        System.out.println(convertFrom);
+        String convertTo = toLabel.getText().toString();
+        System.out.println(convertTo);
+        double amount = Double.parseDouble(fromAmount.getText().toString());
+        System.out.println(amount);
+        conversion.sendHttpGetRequest(convertFrom, convertTo, amount);
+    }
 }
+
+
+//TODO
+// 1. Add a button to the conversion activity that will take the user back to the main activity
+// 2. figure out how to display the conversion result in the provided TextView
+// 3. Move networking code to a separate class
